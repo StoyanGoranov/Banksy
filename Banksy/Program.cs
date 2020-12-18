a463abee69e05ed09e9f7c733dcf0e03cdd37758
Stoyan Goranov <35375994+StoyanGoranov@users.noreply.github.com>
2020-12-18 09:57:46 +0200

    Add project files.

---
+++ Banksy/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Banksy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                BankAccount account = new BankAccount("Krum", 10000);
+                Console.WriteLine($"Account {account.Number} was created for {account.Owner} with ${account.Balance}.");
+
+                account.MakeWithdrawal(59.99m, DateTime.Now, "Seeds");
+
+                Console.WriteLine(account.GetAccountHistory());
+
+
+            } 
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine("Exception caught trying to deposit/withdraw negative amount");
+                Console.WriteLine(e.ToString());
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine("Exception caught trying to overdraw");
+                Console.WriteLine(e.ToString());
+            }    
+            
+        }
+    }
+}
